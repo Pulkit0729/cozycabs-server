875fd2676cf1886fc3c8e4525d258f01c0b02658
Pulkit0729 <myalapi2022@gmail.com>
2024-06-27 19:28:29 +0530

    bug

--- src/services/handlers.ts
+++ src/services/handlers.ts
@@ -25,7 +25,7 @@ export async function bookFromBlabla(user_phone: string, user_name: string, seat
         if (!ride) throw new Error("No ride found");
         let bookings = await Booking.find({ user_no: user_phone, is_cancelled: false });
         if (bookings.length > 0) {
-            await sendToUser(eventType.booking_exists, { user_no: user_phone, user_name, from: ride.from_address, to: ride.to_address, departure_time: ride.departure_time, arrival_time: ride.arrival_time });
+            await sendToUser(eventType.booking_exists, { user_no: user_phone, user_name, date: ride.date,  from: ride.from_address, to: ride.to_address, departure_time: ride.departure_time, arrival_time: ride.arrival_time });
             let admins = await Admin.find();
             for (let admin of admins) {
                 await sendToAdmin(eventType.booking_exists, { user_no: user_phone, user_name, from: ride.from_address, to: ride.to_address, departure_time: ride.departure_time, arrival_time: ride.arrival_time }, admin.phone);

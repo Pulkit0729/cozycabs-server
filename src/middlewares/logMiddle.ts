19d3b3ea827c555afa49664aac28f02e9475c16f
vikramsingh117 <vikramandanshu@gmail.com>
2024-08-22 20:14:39 +0530

    ITN-7: added logger
    Changes implemented

---
+++ src/middlewares/logMiddle.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import logger from '../logger/logger';
+
+const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  res.on('finish', () => {
+    const logDetails = {
+      additionalInfo: {
+        request:{
+          ip: req.ip,
+          method: req.method,
+          body: req.body,
+          url: req.originalUrl,
+        },
+        response:{
+          statusCode: res.statusCode,
+        },
+      },
+      timestamp: new Date().toISOString(),
+    };
+    logger.info('API', logDetails);
+  });
+  next();
+};
+
+export default logMiddleware;

b1ada8528e8559b67c0e3dd1f8dd69f898f020c4
Pulkit0729 <myalapi2022@gmail.com>
2024-08-27 18:27:23 +0530

    fix(admin): notifications

--- src/routes/v1/booking/booking.controller.ts
+++ src/routes/v1/booking/booking.controller.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
 import { BookingService } from '../../../services/booking.service';
 import { BookingChannel } from '../../../utils/constants';
+import { getBooking } from '../../../dal/booking.dal';
+import {
+  sendMessage,
+  SendPulseEventTypes,
+} from '../../../services/external/sendpulse';
+import Admin from '../../../models/admin';
 
 export class BookingControlller {
   static async book(req: Request, res: Response) {
@@ -12,12 +18,46 @@ export class BookingControlller {
       BookingChannel.app,
       userPromoId
     );
+    if (response.success && response.booking) {
+      const booking = await getBooking(response.booking?.bookingId.toString());
+      if (booking) {
+        const admins = await Admin.find();
+        for (const admin of admins) {
+          await sendMessage(
+            SendPulseEventTypes.BOOK,
+            'admin',
+            user,
+            booking?.ride.driver,
+            admin,
+            booking,
+            booking?.ride
+          );
+        }
+      }
+    }
     return res.json(response);
   }
 
   static async cancel(req: Request, res: Response) {
     const { user, bookingId } = req.body;
     const response = await BookingService.cancel(user, bookingId);
+    if (response.success && response.data) {
+      const booking = await getBooking(response.data?.bookingId.toString());
+      if (booking) {
+        const admins = await Admin.find();
+        for (const admin of admins) {
+          await sendMessage(
+            SendPulseEventTypes.CANCEL,
+            'admin',
+            user,
+            booking?.ride.driver,
+            admin,
+            booking,
+            booking?.ride
+          );
+        }
+      }
+    }
     return res.json(response);
   }
 }

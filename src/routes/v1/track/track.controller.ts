5c2ca848271a9d208eaf80b1e850ff9557a40d8e
Pulkit0729 <myalapi2022@gmail.com>
2024-09-03 15:55:02 +0530

    feat: retool workflows

--- src/routes/v1/track/track.controller.ts
+++ src/routes/v1/track/track.controller.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from 'express';
 import logger from '../../../logger/logger';
 import { sendMessageToGroup } from '../../../services/external/telegram';
+import { triggerWorkflow } from '../../../services/external/retool';
 
 export default class TrackController {
   static async book(req: Request, res: Response) {
     try {
-      await sendMessageToGroup(
-        'Book Request' + Object.values(req.body).join(' ')
-      );
+      await Promise.all([
+        sendMessageToGroup('Book Request' + Object.values(req.body).join(' ')),
+        triggerWorkflow(req.body),
+      ]);
     } catch (error: any) {
       logger.error(error.message);
     }

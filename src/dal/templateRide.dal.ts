c1d2163b1bd7da16003f2fa17b1f5ef690550f65
Pulkit0729 <myalapi2022@gmail.com>
2024-09-27 18:35:23 +0530

    fix: rides metadata

--- src/dal/templateRide.dal.ts
+++ src/dal/templateRide.dal.ts
@@ -1,8 +1,12 @@
-import TemplatedRide from '../models/templatedRides';
+import TemplatedRide, { TemplateRideStatus } from '../models/templatedRides';
 
 export async function getUniqueFromToPlaces() {
-  const from = await TemplatedRide.distinct('from');
-  const to = await TemplatedRide.distinct('to');
+  const from = await TemplatedRide.distinct('from', {
+    status: TemplateRideStatus.ACTIVE,
+  });
+  const to = await TemplatedRide.distinct('to', {
+    status: TemplateRideStatus.ACTIVE,
+  });
   from.filter((f) => f != null);
   to.filter((t) => t != null);
   return { from, to };
